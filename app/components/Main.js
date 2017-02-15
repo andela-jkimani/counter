bd4e431677f66dd10304c54bfeefa3b0152e3967
andela-jkimani <jackline.kimani@andela.com>
2017-02-15 23:20:50 +0300

    add hello component

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<Hello name="Jacky" />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,19 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './public/index.html',
   filename: 'index.html',
   inject: 'body'
 })
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './app/components/Main.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'public/bundle.js'
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
+      {test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
