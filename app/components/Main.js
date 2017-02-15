d9cc64982bd241ddbd39d5520f569e1a1d26df6f
andela-jkimani <jackline.kimani@andela.com>
2017-02-16 01:44:14 +0300

    add onClick event

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,8 +4,12 @@ var ReactDOM = require('react-dom');
 var CounterDisplay = React.createClass({
   render: function () {
     return (
-      <div>{this.props.counterProp}</div>
-    )
+      <div>
+        <div>{this.props.counterProp}</div>
+        <button onClick={this.props.incrementCounter}>+</button>
+        <button onClick={this.props.decrementCounter}>-</button>
+      </div>
+    );
   }
 })
 
@@ -15,13 +19,26 @@ var Counter = React.createClass({
       counter: 0
     }
   },
+
+  handleIncrement: function () {
+    this.setState({counter: this.state.counter + 1});
+  },
+
+  handleDecrement: function() {
+    this.setState({counter: this.state.counter - 1});
+  },
+
   render: function() {
     return (
       <div>
         <h2>{this.props.name}</h2>
-        <CounterDisplay counterProp={this.state.counter}/>
+        <CounterDisplay
+          counterProp={this.state.counter}
+          incrementCounter={this.handleIncrement}
+          decrementCounter={this.handleDecrement}
+        />
       </div>
-    )
+    );
   }
 });
 
