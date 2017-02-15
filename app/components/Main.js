43fb7ab85795e8331506625f7329281167ee5209
andela-jkimani <jackline.kimani@andela.com>
2017-02-16 01:12:01 +0300

    add props and state

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+var CounterDisplay = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.counterProp}</div>
+    )
+  }
+})
+
+var Counter = React.createClass({
+  getInitialState: function () {
+    return {
+      counter: 0
+    }
+  },
   render: function() {
     return (
       <div>
-        Hello {this.props.name}
+        <h2>{this.props.name}</h2>
+        <CounterDisplay counterProp={this.state.counter}/>
       </div>
     )
   }
 });
 
-ReactDOM.render(<Hello name="Jacky" />, document.getElementById('app'));
+ReactDOM.render(<Counter name="Counter Display" />, document.getElementById('app'));
